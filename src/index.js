ec485f6d35822fdf4f258178776c7d357ea10d4c
ke6ch <19749261+ke6ch@users.noreply.github.com>
2019-10-22 09:45:18 +0900

    Add SignIn & SearchList

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React, { Component } from "react";
-import * as ReactDOM from "react-dom";
+import React from 'react'
+import * as ReactDOM from 'react-dom'
+import App from './App'
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello World</h1>
-      </div>
-    );
-  }
-};
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>Hello World</h1>
+//         <App />
+//       </div>
+//     );
+//   }
+// }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'))
